2105181174ab178dda2f96f4242e59f2f7bdc3ae
Hugo van de Vliert <hugovdv@outlook.com>
2019-02-20 17:28:06 +0100

    Added Spring data dependency - added Repositories

---
+++ src/main/java/nl/fontys/kwetter/repository/KweetRepository.java
@@ -0,0 +1,7 @@
+package nl.fontys.kwetter.repository;
+
+import org.springframework.data.repository.Repository;
+import nl.fontys.kwetter.models.Kweet;
+
+public class KweetRepository implements Repository<Kweet, Integer> {
+}
---
+++ src/main/java/nl/fontys/kwetter/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package nl.fontys.kwetter.repository;
+
+import org.springframework.data.repository.Repository;
+import nl.fontys.kwetter.models.User;
+
+public class UserRepository implements Repository<User, Integer> {
+}

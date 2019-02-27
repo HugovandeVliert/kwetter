e1a145cd3eae4ca4550b443c0c11ac4467cb6de0
Hugo van de Vliert <hugovdv@outlook.com>
2019-02-27 12:37:20 +0100

    Added Spring Boot init file

---
+++ src/main/java/nl/fontys/kwetter/Kwetter.java
@@ -0,0 +1,11 @@
+package nl.fontys.kwetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kwetter {
+    public static void main(String[] args) {
+        SpringApplication.run(Kwetter.class, args);
+    }
+}

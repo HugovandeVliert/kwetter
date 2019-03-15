2ed0ecf078234280a78e414b02a42a18c1a5c423
Hugo van de Vliert <hugovdv@outlook.com>
2019-03-15 10:14:13 +0100

    Fixed deployment on local payara server

--- src/main/java/nl/fontys/kwetter/Kwetter.java
+++ src/main/java/nl/fontys/kwetter/Kwetter.java
@@ -2,9 +2,17 @@ package nl.fontys.kwetter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Kwetter {
+public class Kwetter extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Kwetter.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Kwetter.class, args);
     }

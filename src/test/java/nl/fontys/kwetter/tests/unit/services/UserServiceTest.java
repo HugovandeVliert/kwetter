7d3debec03ce89a5348c100ee9edab6cff63391d
Hugo van de Vliert <hugovdv@outlook.com>
2019-03-18 22:19:18 +0100

    Added testing of user service logic

--- src/test/java/nl/fontys/kwetter/tests/unit/services/UserServiceTest.java
+++ src/test/java/nl/fontys/kwetter/tests/unit/services/UserServiceTest.java
@@ -10,9 +10,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Transactional
 @SpringBootTest(properties = "spring.profiles.active=test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class UserServiceTest {
@@ -68,5 +72,64 @@ class UserServiceTest {
         assertEquals(user1.getId(), 1);
         assertEquals(user1.getName(), "User 1");
         assertEquals(user1.getUsername(), "user1");
+    }
+
+    @Test
+    void findAllTest() throws ModelValidationException {
+        User user1 = userService.save(mockData.createUser("User 1", Role.USER));
+        User user2 = userService.save(mockData.createUser("User 2", Role.USER));
+
+        List<User> foundUsers = userService.findAll();
+
+        assertEquals(foundUsers.get(0), user1);
+        assertEquals(foundUsers.get(1), user2);
+    }
+
+    @Test
+    void deleteUserTest() throws ModelValidationException, ModelNotFoundException {
+        User user1 = userService.save(mockData.createUser("User 1", Role.USER));
+
+        assertEquals(userService.find(user1.getId()), user1);
+
+        userService.delete(user1.getId());
+
+        assertThrows(ModelNotFoundException.class, () -> userService.find(user1.getId()));
+    }
+
+    @Test
+    void followUserTest() throws ModelValidationException, ModelNotFoundException {
+        User user1 = userService.save(mockData.createUser("User 1", Role.USER));
+        User follower1 = userService.save(mockData.createUser("Follower 1", Role.USER));
+        User follower2 = userService.save(mockData.createUser("Follower 2", Role.USER));
+
+        assertTrue(userService.getFollowers(user1.getId()).isEmpty());
+
+        userService.addFollower(user1.getId(), follower1.getId());
+        userService.addFollower(user1.getId(), follower2.getId());
+
+        List<User> followers = userService.getFollowers(user1.getId());
+
+        assertFalse(followers.isEmpty());
+        assertTrue(followers.contains(follower1));
+        assertTrue(followers.contains(follower2));
+    }
+
+    @Test
+    void unFollowUserTest() throws ModelValidationException, ModelNotFoundException {
+        User user1 = userService.save(mockData.createUser("User 1", Role.USER));
+        User follower1 = userService.save(mockData.createUser("Follower 1", Role.USER));
+        User follower2 = userService.save(mockData.createUser("Follower 2", Role.USER));
+
+        userService.addFollower(user1.getId(), follower1.getId());
+        userService.addFollower(user1.getId(), follower2.getId());
+
+        List<User> followers = userService.getFollowers(user1.getId());
+
+        assertFalse(followers.isEmpty());
+
+        userService.removeFollower(user1.getId(), follower1.getId());
+        userService.removeFollower(user1.getId(), follower2.getId());
+
+        assertTrue(followers.isEmpty());
     }
 }

32b4dd340d37cd92c033517226d0e292dcb02979
Hugo van de Vliert <hugovdv@outlook.com>
2019-03-22 09:58:45 +0100

    Removed unnecessary array

--- src/main/java/nl/fontys/kwetter/exceptions/ApiControllerExceptionHandler.java
+++ src/main/java/nl/fontys/kwetter/exceptions/ApiControllerExceptionHandler.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ApiControllerExceptionHandler {
-    @ExceptionHandler({ModelNotFoundException.class})
+    @ExceptionHandler(ModelNotFoundException.class)
     public ResponseEntity handleModelNotFoundException(ModelNotFoundException e) {
         System.out.println("An Exception has occurred: " + e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler({ModelValidationException.class})
+    @ExceptionHandler(ModelValidationException.class)
     public ResponseEntity handleModelValidationException(ModelValidationException e) {
         System.out.println("An Exception has occurred: " + e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);

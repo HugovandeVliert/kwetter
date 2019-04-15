e19c853f2e7d91605216b3cd6bbfe9003a3d33cc
Hugo van de Vliert <hugovdv@outlook.com>
2019-04-16 00:27:53 +0200

    Modified api login url

--- src/main/java/nl/fontys/kwetter/security/SecurityConstants.java
+++ src/main/java/nl/fontys/kwetter/security/SecurityConstants.java
@@ -6,5 +6,5 @@ class SecurityConstants {
     static final String TOKEN_PREFIX = "Bearer ";
     static final String HEADER_STRING = "Authorization";
     static final String SIGN_UP_URL = "/api/users";
-    static final String LOG_IN_URL = "/api/login";
+    static final String LOG_IN_URL = "/api/users/login";
 }

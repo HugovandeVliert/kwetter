dfba171bca8ffea0359cc994efc4db6500b9f60f
Hugo van de Vliert <hugovdv@outlook.com>
2019-04-16 16:31:46 +0200

    Refactor angular imports

--- angularclient/src/app/_services/user.service.ts
+++ angularclient/src/app/_services/user.service.ts
@@ -1,8 +1,8 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from "../../environments/environment";
 
-import {User} from "../_models/user";
-import {environment} from "../../environments/environment";
+import { User } from "../_models/user";
 
 @Injectable({providedIn: 'root'})
 export class UserService {

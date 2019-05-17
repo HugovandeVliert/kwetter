e171b9d51ce6cf2827055f965488728a74b8147e
Hugo van de Vliert <hugovdv@outlook.com>
2019-05-17 12:30:04 +0200

    Changed system outs to logger

--- src/main/java/nl/fontys/kwetter/exceptions/ApiControllerExceptionHandler.java
+++ src/main/java/nl/fontys/kwetter/exceptions/ApiControllerExceptionHandler.java
@@ -1,28 +1,30 @@
 package nl.fontys.kwetter.exceptions;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Log4j2
 @ControllerAdvice
 public class ApiControllerExceptionHandler {
     @ExceptionHandler(ModelNotFoundException.class)
     public ResponseEntity handleModelNotFoundException(ModelNotFoundException e) {
-        System.out.println("An Exception has occurred: " + e.getMessage());
+        log.error("An exception has occurred: " + e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(UsernameNotFoundException.class)
     public ResponseEntity handleModelNotFoundException(UsernameNotFoundException e) {
-        System.out.println("An Exception has occurred: " + e.getMessage());
+        log.error("An exception has occurred: " + e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ModelValidationException.class)
     public ResponseEntity handleModelValidationException(ModelValidationException e) {
-        System.out.println("An Exception has occurred: " + e.getMessage());
+        log.error("An exception has occurred: " + e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
     }
 }

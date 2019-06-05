af83c2e25186fe6f501acea559e06e2975c3d0d5
Hugo van de Vliert <hugovdv@outlook.com>
2019-06-05 21:27:05 +0200

    Added jsdoc to service

--- api/src/main/java/nl/fontys/kwetter/services/interfaces/IWebSocketService.java
+++ api/src/main/java/nl/fontys/kwetter/services/interfaces/IWebSocketService.java
@@ -4,8 +4,22 @@ import nl.fontys.kwetter.models.User;
 
 import java.util.List;
 
+/**
+ * The interface Web socket service.
+ */
 public interface IWebSocketService {
+    /**
+     * Send timeline update notification to all given followers.
+     *
+     * @param followers the followers
+     */
     void sendTimelineUpdateNotification(List<User> followers);
 
+    /**
+     * Send new follower notification to the given receiver.
+     *
+     * @param receiver the receiver
+     * @param follower the follower
+     */
     void sendNewFollowerNotification(User receiver, User follower);
 }
